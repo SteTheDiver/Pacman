9d64499f942567d257e97b8c473a5d952c47a0cc
SteTheDiver <ste@name.com>
2020-10-02 10:22:07 +0200

    update class

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,31 @@
 "use strict";
 
-
 class Pacman {
-    constructor() {
-this.xpos = 0;
-this.mouth = 0;
-    }
+  constructor() {
+    this.xpos = 0;
+    this.mouth = 0;
+  }
+
+  render() {
+    const elm = document.createElement('div');
+    elm.className = "entity entity--pac pacboy-active-light";
+
+    return elm;
+    
+  }
+
+  mount(parent) {
+      this.element = this.render();
+      parent.appendChild(this.element);  
+  }
+
+  update() {}
 }
+
+const app = document.querySelector('#app');
+const pacMan1 = new Pacman;
+
+pacMan1.mount(app)
 
 
 let xpos = 100;
@@ -16,7 +35,7 @@ document.addEventListener("keydown", (event) => {
   const entity = document.querySelector(".entity");
   if (event.code === "ArrowRight") {
     xpos += TILE_SIZE;
-    entity.style.left = xpos + 'px';
+    entity.style.left = xpos + "px";
   }
 });
 

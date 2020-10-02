4df6c9e0608f091208fb24c75e540e5b7b71b259
Aurelien Gaillard <fakeemail@gadzo.org>
2020-10-02 10:57:13 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,31 @@
 "use strict";
+const TILE_SIZE = 85;
 
 class Pacman {
   constructor() {
     this.xpos = 0;
+    this.ypos = 0
     this.mouth = 0;
+    this.facing = 0;
   }
 
   render() {
     const elm = document.createElement('div');
     elm.className = "entity entity--pac pacboy-active-light";
-
+    document.addEventListener("keydown", (event) => {
+      if (event.code === "ArrowRight") {
+        this.moveRight()
+      }
+      if (event.code === "ArrowLeft") {
+        this.moveLeft()
+      }
+      if (event.code === "ArrowDown") {
+        this.moveDown()
+      }
+      if (event.code === "ArrowUp") {
+        this.moveUp()
+      }
+    });
     return elm;
     
   }
@@ -19,7 +35,42 @@ class Pacman {
       parent.appendChild(this.element);  
   }
 
-  update() {}
+  update() {
+    //this.element.classList.toggle('pacboy-active-light--mouth')
+    this.element.style.backgroundPositionY = this.facing+'%'
+    if (this.mouth === 0) {
+      this.mouth = 100
+    }
+    else {
+      this.mouth = 0
+    }
+    this.element.style.backgroundPositionX = this.mouth+'%'
+  }
+
+  moveRight() {
+    this.xpos += TILE_SIZE;
+    this.element.style.left = this.xpos + "px";
+    this.facing = 0
+    this.update()
+  }
+  moveLeft() {
+    this.xpos -= TILE_SIZE;
+    this.element.style.left = this.xpos + "px";
+    this.facing = 33
+    this.update()
+  }
+  moveUp() {
+    this.ypos -= TILE_SIZE;
+    this.element.style.top = this.ypos + "px";
+    this.facing = 100
+    this.update()
+  }
+  moveDown() {
+    this.ypos += TILE_SIZE;
+    this.element.style.top = this.ypos + "px";
+    this.facing = 66
+    this.update()
+  }
 }
 
 const app = document.querySelector('#app');
@@ -27,16 +78,5 @@ const pacMan1 = new Pacman;
 
 pacMan1.mount(app)
 
-
-let xpos = 100;
-const TILE_SIZE = 85;
-
-document.addEventListener("keydown", (event) => {
-  const entity = document.querySelector(".entity");
-  if (event.code === "ArrowRight") {
-    xpos += TILE_SIZE;
-    entity.style.left = xpos + "px";
-  }
-});
 
 console.log("it works!");
